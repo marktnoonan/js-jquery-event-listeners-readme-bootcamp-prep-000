657f36f35bd381a31831df5b4c06a21c72827f0d
Mark Noonan <markthomasnoonan@gmail.com>
2018-06-10 22:59:29 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,31 @@
 //define functions here
-
+function getIt() {
+  $("p").on("click", function () {
+    alert("Hey!")
+  })
+}
+function frameIt() {
+  $("img").on("load", function () {
+    $("img").addClass("tasty")
+  })
+}
+function pressIt() {
+  $(document).on("keydown", function (keypressEvent) {
+    if (keypressEvent.which == 71){
+      alert("G!")      
+    }
+  })
+}
+function submitIt() {
+  $("form").on("submit", function(){
+    alert("Your form is going to be submitted now.")
+  })
+}
 $(document).ready(function(){
 
 // call functions here
-
+getIt()
+frameIt()
+pressIt()
+submitIt()
 });
